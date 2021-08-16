b4f130d207a26bc2ca5c17539bb7af53fc58ae1b
Abdullah Zubair <metin2grafics@gmail.com>
2021-08-15 23:01:38 -0400

    Add amazon cognito support

--- test/test.js
+++ test/test.js
@@ -1,8 +1,43 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+const chai = require('chai');
+const chaiHttp = 'chai-http';
+const app = '../src/RecipesLambda';
+
+chai.use(chaiHttp);
+chai.should();
+
+describe("Recipes", () => {
+  describe("GET /", () => {
+      // Test to get all students record
+      it("should get all recipes", (done) => {
+           chai.request(app)
+               .get('/recipes')
+               .end((err, res) => {
+                   res.should.have.status(200);
+                   res.body.should.be.a('object');
+                   done();
+                });
+       });
+      // Test to get single student record
+      it("should get a single student record", (done) => {
+           const id = 1;
+           chai.request(app)
+               .get(`/${id} `)
+               .end((err, res) => {
+                   res.should.have.status(200);
+                   res.body.should.be.a('object');
+                   done();
+                });
+       });
+       
+      // Test to get single student record
+      it("should not get a single student record", (done) => {
+           const id = 5;
+           chai.request(app)
+               .get(`/${id}`)
+               .end((err, res) => {
+                   res.should.have.status(404);
+                   done();
+                });
+       });
   });
 });
